91404e186a58664a6336ea22a262fb9823e7a7e3
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2019-03-17 01:01:55 +0900

    Add `lifecycle-viewmodel-savedstate` module

---
+++ lifecycle-viewmodel-savedstate/src/main/java/com/github/tmurakami/aackt/lifecycle/SavedStateVMCreator.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Tsuyoshi Murakami
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tmurakami.aackt.lifecycle
+
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateVMFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
+
+/**
+ * An interface that instantiate [ViewModel]s.
+ */
+interface SavedStateVMCreator {
+    /**
+     * Creates a new [ViewModel] of the given [modelClass]. You can write and read values to and
+     * from the saved state using the given [handle].
+     */
+    fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle): T
+}
+
+/**
+ * Creates a new [ViewModelProvider.Factory] with the given [owner] and [defaultArgs].
+ */
+fun SavedStateVMCreator.toFactory(
+    owner: SavedStateRegistryOwner,
+    defaultArgs: Bundle? = null
+): ViewModelProvider.Factory = object : AbstractSavedStateVMFactory(owner, defaultArgs) {
+    override fun <T : ViewModel> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T = this@toFactory.create(key, modelClass, handle)
+}
---
+++ lifecycle-viewmodel-savedstate/src/test/java/com/github/tmurakami/aackt/lifecycle/viewmodel/savedstate/SavedStateVMCreatorTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 Tsuyoshi Murakami
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tmurakami.aackt.lifecycle.viewmodel.savedstate
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import com.github.tmurakami.aackt.lifecycle.SavedStateVMCreator
+import com.github.tmurakami.aackt.lifecycle.toFactory
+import kotlin.test.Test
+
+class SavedStateVMCreatorTest {
+    @Test
+    fun getViewModel() {
+        class TestViewModel(@Suppress("UNUSED_PARAMETER") handle: SavedStateHandle) : ViewModel()
+
+        val owner = object : SavedStateRegistryOwner {
+            private val lifecycle = LifecycleRegistry(this)
+            private val controller = SavedStateRegistryController.create(this).apply {
+                performRestore(null)
+            }
+
+            override fun getLifecycle(): Lifecycle = lifecycle
+            override fun getSavedStateRegistry(): SavedStateRegistry = controller.savedStateRegistry
+        }
+        val factory = object : SavedStateVMCreator {
+            override fun <T : ViewModel> create(
+                key: String,
+                modelClass: Class<T>,
+                handle: SavedStateHandle
+            ): T = modelClass.cast(TestViewModel(handle))!!
+        }.toFactory(owner)
+        ViewModelProvider(ViewModelStore(), factory).get("testViewModel", TestViewModel::class.java)
+    }
+}
