3a2aa4822293ea176b257d0788c9cb0ee23c9be1
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2019-03-27 19:28:18 +0900

    Reformat

--- lifecycle-livedata/src/main/java/androidx/lifecycle/ChangesOnlySubscriptionImpl.kt
+++ lifecycle-livedata/src/main/java/androidx/lifecycle/ChangesOnlySubscriptionImpl.kt
@@ -21,8 +21,7 @@ import com.github.tmurakami.aackt.lifecycle.Subscription
 internal class ChangesOnlySubscriptionImpl<T>(
     private val data: LiveData<T>,
     private val observer: Observer<in T>
-) : Subscription,
-    Observer<T> {
+) : Subscription, Observer<T> {
     private val startVersion = data.version
 
     override fun onChanged(t: T) {
