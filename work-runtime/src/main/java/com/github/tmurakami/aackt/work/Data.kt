a5c02925a85c0f72744facfb39095e2bc8411eb0
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2019-03-29 20:12:42 +0900

    Added `Data.kt`

---
+++ work-runtime/src/main/java/com/github/tmurakami/aackt/work/Data.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018 Tsuyoshi Murakami
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tmurakami.aackt.work
+
+import androidx.work.Data
+import kotlin.reflect.KProperty
+
+/**
+ * Returns a value associated with the [property] name.
+ */
+inline operator fun <reified T> Data.getValue(thisRef: Any?, property: KProperty<*>): T =
+    keyValueMap[property.name] as T
